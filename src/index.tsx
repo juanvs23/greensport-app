20b17d4f59d097db5da372f132e7d7abc9cbfa28
Juan Carlos Avila <juanvs23@gmail.ocm>
2022-01-06 13:06:00 -0400

    subir

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ import  { GlobalStyles } from './styles/styles';
 ReactDOM.render(
   
   <Provider store={store}>
-    <Router basename={process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_API_URL : '/'}>
+    <Router>
     
         <GlobalStyles  />
         <App />
